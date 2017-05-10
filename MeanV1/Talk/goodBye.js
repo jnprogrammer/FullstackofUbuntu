31f46d69d55baf86293f99d49cafb061be067f8a
Josh <joshuanparker@gmail.com>
2017-05-09 20:01:14 -0400

    WarmUps

---
+++ MeanV1/Talk/goodBye.js
@@ -0,0 +1,3 @@
+module.exports = function(){
+    console.log("goodBye User . .. thats a pretty old saying, like damn its 2017");
+}
--- MeanV1/app.js
+++ MeanV1/app.js
@@ -1,1 +1,7 @@
 require('./sayHi.js');
+var question = require('./talk/question');
+var goodbye = require("./Talk/goodBye");
+var talk = require('./Talk');
+talk.intro();
+talk.hello("Josh");
+goodbye();
