b61a3e292e422def5396cc571a602f7b592be955
Josh <joshuanparker@gmail.com>
2017-05-09 20:18:01 -0400

    adding an important question

--- MeanV1/app.js
+++ MeanV1/app.js
@@ -1,7 +1,13 @@
 require('./sayHi.js');
+
 var question = require('./talk/question');
 var goodbye = require("./Talk/goodBye");
 var talk = require('./Talk');
+
 talk.intro();
 talk.hello("Josh");
+
+var answer = question.ask("What is the meaning of life?");
+console.log(answer);
+
 goodbye();
