b2682af6f6f8d4c5b7f88f1a9692bd3d3342cc50
Josh <joshuanparker@gmail.com>
2017-06-04 18:24:23 -0400

    First draft of app.js

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+app.set('port',3000);
+
+app.use(express.static(path.join(__dirname,'public')));
+
+//app.use('/public',express.static(path.join(__dirname,'public')));
+
+app.get('/json',function(req,res){
+    console.log("GET the json");
+    res
+        .status(200)
+        .json({"jsonData" : true});
+});
+
+app.get('/file',function(req,res){
+    console.log("GET the json");
+    res
+        .status(200)
+        .sendFile(path.join(__dirname,'app.js'));
+});
+
+var server = app.listen(app.get('port'),function(){
+    var port = server.address().port;
+    console.log("Where things are happening " + port);
+});
+
+
