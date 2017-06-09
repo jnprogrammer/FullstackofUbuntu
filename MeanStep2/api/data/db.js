cf4c7a3b0fb5ebeac325a7d9ec3ee4790c7cf848
Josh <joshuanparker@gmail.com>
2017-06-08 21:30:58 -0400

    switched to mongoose connection using db.js

---
+++ MeanStep2/api/data/db.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+var dburl = 'mongodb://localhost:27017/meanhotel';
+
+mongoose.connect(dburl);
+
+mongoose.connection.on('connected',function(){
+    console.log('Mongoose connected to '+dburl);
+});
+
+mongoose.connection.on('disconnected',function(){
+    console.log('Mongoose disconnected to '+dburl);
+});
+
+mongoose.connection.on('error',function(){
+    console.log('Mongoose connection error: '+dburl);
+});
